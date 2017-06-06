6e9244a79256445fe746d4ca1dc90ba83d7c0859
jeanwei <jeanwei938@msn.com>
2017-06-06 18:33:43 +0800

    Add navigating_site feature test

--- features/navigating_the_site.feature
+++ features/navigating_the_site.feature
@@ -6,18 +6,18 @@ Feature: Navigating the Site
   Background:
 
   Scenario: see a photo at home page
-    Given I visit "home" page
-    Then I should see a photo
+    Given I visit 'home' page
+    Then I should see photo
 
   Scenario: see paragraphs at about me page
-    Given I visit "about me" page
+    Given I visit 'about me' page
     Then I should see paragraphs
 
   Scenario: see a youtube video
-    Given I visit "video submission" page
+    Given I visit 'video submission' page
     Then I should see a youtube video
 
   Scenario: see a contact email and link to github
-    Given I visit "contact me" page
-    Then I should see an email
-    And I should see a link to github
+    Given I visit 'contact me' page
+    Then I should see a contact form
+    And I should see a github link
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -14,6 +14,54 @@ def path_to(page_name)
   end
 end
 
-Given(/^I visit "(.*?)" page$/) do |page|
+Given(/^I visit '(.*?)' page$/) do |page|
   visit path_to(page)
+end
+
+Given(/^I am at '(.*?)' page$/) do |page|
+  visit path_to(page)
+end
+
+Then(/^I should see photo$/) do
+  expect(page).to have_css 'img'
+end
+
+Then(/^I should see paragraphs$/) do
+  expect(page).to have_css 'p'
+end
+
+Then(/^I should see a youtube video$/) do
+  expect(page).to have_css '[src^="https://www.youtube.com/embed/"]'
+end
+
+Then(/^I should see a github link$/) do
+  expect(page).to have_css 'a[href^="https://github.com/"]'
+end
+
+Then(/^I should see a contact form$/) do
+  expect(page).to have_css 'form'
+end
+
+Then(/^I should see '([^']*)'$/) do |value|
+  expect(page).to have_content value
+end
+
+Then(/^I should not see '([^']*)'$/) do |value|
+  expect(page).not_to have_content value
+end
+
+Then(/^I click '([^']*)' link$/) do |link|
+  click_link link
+end
+
+Then(/^I open '([^']*)' drop down link/) do |link|
+  click_link link
+end
+
+Then(/^I fill in '([^']*)' with '([^']*)'/) do |field, value|
+  fill_in field, with: value
+end
+
+Then(/^I click '([^']*)' button$/) do |button|
+  click_button button
 end
