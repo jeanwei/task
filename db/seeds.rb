981d9eaa8677b98f218b81d78fda7a5ac6b5787a
jeanwei <jeanwei938@msn.com>
2017-06-06 18:34:54 +0800

    Seed database with blog post

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,54 @@
 
 
 Owner.create(email: 'snoopy@example.com', password: 'iamsnoopy', password_confirmation: 'iamsnoopy')
+Blog.create(title: 'Lorem Ipsum', body: 'Lorem ipsum dolor sit amet, usu graecis recusabo no, mea option explicari ei,
+et duo impetus quaestio moderatius. His ipsum persius ne. Eu eum deseruisse vituperatoribus. Nam ei consul liberavisse
+interpretaris. Alienum legendos scribentur mei ea, eam ad inimicus sententiae. Rebum volumus adolescens an est, error
+quodsi vulputate an ius, in has porro laudem quaerendum.An summo explicari philosophia mel, eos ad decore legimus
+scribentur, decore commune cu pro. Nibh soleat ut eam, te numquam complectitur consectetuer cum, bonorum scribentur
+complectitur usu ad. Pri oratio maiorum suavitate in. Per ea quis latine phaedrum, ex vis persius inermis dissentiunt.
+Ut cum assum integre.
+
+Ea mei vivendo legendos. No latine fuisset nec. Complectitur vituperatoribus ea quo, vix ut prima iriure, in altera
+nostro invidunt sea. Meis oblique concludaturque sit an. Cum et munere indoctum, purto nulla pertinax pri at, enim
+partem aperiam et qui.
+
+Eos no habeo integre legendos, no sed amet democritum. Vim mundi tibique rationibus id, audire saperet molestie est te.
+Erant eruditi repudiare ex usu, debet feugait accommodare cu sit. Feugait scripserit ex ius, ne per vocent sensibus
+adversarium. Cu omnis vituperata his, an sed postea comprehensam, vix cu odio unum quaestio. Sea at illud equidem
+electram. Cu suas enim veritus mea, cu possit commodo blandit usu.
+
+Pro omittam imperdiet persecuti ea, nam an prompta mediocritatem, sea commune recteque ut. Cu dicunt vulputate per. Quo
+aeque accumsan atomorum id, et inani accusam cum, eu modus virtute vocibus eos. Has id congue melius denique, quando
+decore labitur ius ut, usu at denique antiopam. Usu ad voluptua disputando, an ius eius posse nominavi. At mutat mundi
+iracundia nec, ex mollis volutpat appellantur qui.')
+
+Blog.create(title: 'Quando aeterno', body: 'Quando aeterno ius ad, quidam veritus maiorum mei et, pro mucius timeam
+alienum ne. Est dicit corpora constituto ne. In his illud congue, interesset persequeris ne quo. Mei aeterno luptatum
+persequeris ex, duo minimum deleniti ea. Debet congue bonorum ne sea, sanctus adversarium liberavisse quo no. Id eum
+probatus perpetua conclusionemque.
+
+Vel utinam principes ei. Te saepe facilis recteque sit, ea ferri facilisis quo. Per animal labitur lobortis no, omnes
+facilisi mediocrem ex eos, vim case hendrerit an. Cum regione percipitur an. In vel eius audiam persequeris, pri
+sapientem scriptorem ex.
+
+Ne nam fierent voluptaria, ut mea solum placerat delicata. Eu vis rebum tritani, has autem mollis et. Case facilisi
+consectetuer ut mel. Vix ei animal eleifend mediocrem, ad has bonorum laoreet, vidit solum tritani per ut. Ea aliquip
+repudiandae signiferumque his.
+
+His vero iusto reprehendunt in, no fabulas intellegam eam. In pri interesset vituperatoribus, movet sadipscing cu his.
+Erant reformidans comprehensam vel ex. Debet recusabo ad mel. Animal periculis mel id, nam qualisque gubergren ad, pro
+in munere omittam perpetua. Nisl impetus per id, at mel meis principes.
+
+Pri omnes mandamus torquatos no. Per ne sumo iusto principes, eu dolor nonumes sea. Nec te offendit constituto, error
+apeirian constituto eam et. Ius in sale facilisis, movet aperiri ut qui.
+
+Id his odio alterum sententiae, an eam option debitis voluptatibus, usu cibo scaevola accusamus ut. Est et viris veniam.
+Ad sea ullum voluptaria, usu ea sint malis appetere. Quo choro bonorum singulis ad, putant probatus concludaturque pri
+an. No libris dictas veritus sea, aliquid dolores has ea, mel diam urbanitas id. Per eu nostro voluptua ocurreret.
+
+His dissentiet suscipiantur at. No pri impedit conclusionemque, no tollit debitis habemus his, vis vocent fabellas
+indoctum in. Te nec idque vulputate definiebas, eius tamquam eligendi et cum. His tamquam aliquando cu. Has tation
+theophrastus cu, omnis dicit accumsan an mei.
+
+Laudem nemore interpretaris ne nec. Eum diam quando id. Te aliquam facilis admodum quo. Nec timeam persius sapientem an.')
